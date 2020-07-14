ebfd5951d9200173e6280c352aa62d201a71debc
Andrii Holtiuk <sssr25@gmail.com>
2020-07-14 20:16:23 +0300

    startng

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php 
 include_once('init.php');
-include_once('templ/header.php');
+include_once('templ/parts/header.php');
 
 
+include_once('templ/pages/home.php');
 
 
-include_once('templ/header.php');
+include_once('templ/parts/header.php');
--- product/index.php
+++ product/index.php
@@ -1,8 +1,9 @@
 <?php 
 include_once('../init.php');
-include_once('../templ/header.php');
+include_once('../templ/parts/header.php');
 
 
+include_once('../templ/pages/prod_list.php');
 
 
-include_once('../templ/header.php');
+include_once('../templ/parts/aheader.php');
